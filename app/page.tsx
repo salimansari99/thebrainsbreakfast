c32ca5f8af1a3c37dd6cc30865a6b07ba8f6d810
Salim Ansari <salim.ansari@nw18.com>
2026-01-19 10:48:13 +0530

    caching fixed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import { connectDB } from "@/lib/db";
 import Thought from "@/models/Thought";
 
+export const revalidate = 600; // 10 minutes caching
+
 export default async function Home() {
   await connectDB();
 
